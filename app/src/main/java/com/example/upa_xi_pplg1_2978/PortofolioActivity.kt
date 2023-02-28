439e3da122ce6c488fcae0514ee149a26d055f1c
BagasSaputra16 <bagasrk727@gmail.com>
2023-02-28 15:40:54 +0700

    first commit

---
+++ app/src/main/java/com/example/upa_xi_pplg1_2978/PortofolioActivity.kt
@@ -0,0 +1,11 @@
+package com.example.upa_xi_pplg1_2978
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PortofolioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_portofolio)
+    }
+}
